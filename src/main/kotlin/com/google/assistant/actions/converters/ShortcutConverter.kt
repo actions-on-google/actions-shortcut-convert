e0f454e7fad4ff6bbc0fc6fc1afdc70d2bfb9e64
Paul Lucas <paullucas@google.com>
2021-06-02 14:44:12 -0400

    Adding intent mapping to shortcut tags
    Change-Id: Ida80b5b76b7da74f7fb526524f8743b93d1243bd

--- src/main/kotlin/com/google/assistant/actions/converters/ShortcutConverter.kt
+++ src/main/kotlin/com/google/assistant/actions/converters/ShortcutConverter.kt
@@ -1,21 +1,59 @@
 package com.google.assistant.actions
 
 import com.google.assistant.actions.model.actions.ActionsRoot
+import com.google.assistant.actions.model.actions.Action
+
 import com.google.assistant.actions.model.shortcuts.*
 
 class ShortcutConverter {
 
-    // TODO(paullucas): add more details
     fun convertActionsToShortcuts(actions: ActionsRoot): List<Shortcut> {
+
+        // Creates a map of actions to entitySetIds for use when creating the <shortcut> tags
+        val actionToEntitySetId = HashMap<String, Action>();
+        actions.actions.forEach { action ->
+            val entitySetId = getEntitySetIdForAction(action)
+            if (entitySetId != null && entitySetId.isNotEmpty()) {
+                actionToEntitySetId[entitySetId] = action
+            }
+        }
+
         val shortcuts: MutableList<Shortcut> = mutableListOf()
         actions.entitySets.map { entitySet ->
-            entitySet.entities.map {entity -> {
-                shortcuts.add(Shortcut(
-                    shortcutId = entity.identifier,
-                    shortcutShortLabel= entity.name  ?: entity.alternateName ?: "",
-                    shortcutLongLabel = entity.name  ?: entity.alternateName ?: ""))
-            } }
+            val matchingAction = actionToEntitySetId[entitySet.entitySetId]
+            // TODO(paullucas): Handle duplicate cases where there may be more intents/action
+            val intent: ShortcutIntent = createIntentFromAction(matchingAction) ?: ShortcutIntent();
+            entitySet.entities.map { entity ->
+                {
+                    val shortcut = Shortcut(
+                        shortcutId = entity.identifier,
+                        shortcutLongLabel = entity.name ?: entity.alternateName ?: "",
+                        intent = listOf(intent)
+                    );
+                    shortcuts.add(shortcut)
+                }
+            }
         }
         return shortcuts
+    }
+
+    private fun createIntentFromAction(action: Action?): ShortcutIntent? {
+        if (action == null) {
+            return null;
+        }
+        // TODO(paullucas): Add fields targetClass, targetPackage and data
+        return ShortcutIntent(action = action.intentName);
+    }
+
+
+    private fun getEntitySetIdForAction(action: Action): String? {
+        action.parameters.forEach { parameter ->
+            if (parameter.entitySetReference != null &&
+                parameter.entitySetReference!!.entitySetId != null) {
+                return parameter.entitySetReference!!.entitySetId
+            }
+            return parameter.entitySetReference?.entitySetId ?: "";
+        }
+        return null;
     }
 }
